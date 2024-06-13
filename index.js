6d397944b10cf1add70e62c57c140bc3ab2c32aa
pranay15155 <pranayprasoon15@gmail.com>
2024-06-13 18:02:04 +0530

    ..

--- index.js
+++ index.js
@@ -6,6 +6,44 @@ const app = express();//variable
 
 const port = 4000;//variable
 
+const githubData = {
+  login: "pranay15155",
+  id: 97624269,
+  node_id: "U_kgDOBdGgzQ",
+  avatar_url: "https://avatars.githubusercontent.com/u/97624269?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/pranay15155",
+  html_url: "https://github.com/pranay15155",
+  followers_url: "https://api.github.com/users/pranay15155/followers",
+  following_url:
+    "https://api.github.com/users/pranay15155/following{/other_user}",
+  gists_url: "https://api.github.com/users/pranay15155/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/pranay15155/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/pranay15155/subscriptions",
+  organizations_url: "https://api.github.com/users/pranay15155/orgs",
+  repos_url: "https://api.github.com/users/pranay15155/repos",
+  events_url: "https://api.github.com/users/pranay15155/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/pranay15155/received_events",
+  type: "User",
+  site_admin: false,
+  name: "PRANAY PRASOON",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 13,
+  public_gists: 0,
+  followers: 1,
+  following: 0,
+  created_at: "2022-01-12T17:35:43Z",
+  updated_at: "2024-05-20T15:13:57Z",
+};
+
 app.get("/", (req, res) => {  //if any request comes at homeroute / then we call a callback function having req and res
   res.send("Hello World!");
 });
@@ -13,6 +51,11 @@ app.get("/", (req, res) => {  //if any request comes at homeroute / then we call
 app.get("/twitter", (req, res) => {
   res.send("<h1>twitter</h1>");
 });
+
+app.get("/github", (req, res) => {
+  res.json(githubData);
+});
+
 app.get("/login", (req, res) => {
   res.send("<h1>pranay prasoon</h1>");
 });
