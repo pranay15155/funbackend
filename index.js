42142d8ba050d9c42e8814590f0b4fe1f3c87da0
pranay15155 <pranayprasoon15@gmail.com>
2024-06-13 16:54:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+require('dotenv').config();
+
+const app = express();//variable
+
+const port = 4000;//variable
+
+app.get("/", (req, res) => {  //if any request comes at homeroute / then we call a callback function having req and res
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Hello twitter!");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>pranay</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
